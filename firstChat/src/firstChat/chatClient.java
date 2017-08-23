2c2c939ec1a7daa69d0c22e1ef5dbbf9259d0ca8
Jack Zhang <smalljjjack@users.noreply.github.com>
2017-08-23 15:43:17 -0400

    Version 0, create frame for client

---
+++ firstChat/src/firstChat/chatClient.java
@@ -0,0 +1,26 @@
+package firstChat;
+
+import java.awt.*;
+
+public class chatClient {
+	public static void main(String[] args) {
+		new chatFrame("Client").launchFrame();
+	}
+}
+
+class chatFrame extends Frame{
+	chatFrame(String client){
+		super("client");
+	}
+	TextField txF = new TextField();
+	TextArea txA = new TextArea();
+	
+	public void launchFrame(){
+		add(txF, BorderLayout.SOUTH);
+		add(txA, BorderLayout.NORTH);
+		setBounds(500, 500, 1500, 1500);
+		pack();
+		setVisible(true);
+
+	}
+}
